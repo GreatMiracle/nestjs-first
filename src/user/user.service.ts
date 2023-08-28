c20b0ad98d0a427c99bd380dec4d1a843abc931b
kien.nguyen4 <kiennguyen16998@gmail.com>
2023-08-28 17:56:45 +0700

    sec 16 time 10m

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,47 @@
 import { Injectable } from "@nestjs/common";
+import { UserEntity } from "./user.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { CreateUserDto } from "./dto/createUser.dto";
+import { sign } from 'jsonwebtoken';
 
 
 @Injectable()
 export class UserService{
-
+    constructor(
+        @InjectRepository(UserEntity)
+        private readonly userRepository: Repository<UserEntity>,
+      ) {}
     
+      async createUser( createUserDto: CreateUserDto ): Promise<UserEntity> {
+
+        const newUser = new UserEntity();
+        Object.assign(newUser, createUserDto);
+        console.log('<<<<<<<<<<========= newUser =========>>>>>>>>', newUser);
+
+        return await this.userRepository.save(newUser);
+      }
+
+      generateJwt(user: UserEntity) :string{
+        return sign(
+            {
+                id: user.id,
+                username: user.username,
+                email: user.email,
+            },
+            "super-secret",
+        )
+      }
+
+      buildUserResponse(user: UserEntity): any {
+        return {
+            userKien :{
+                ...user,
+                token: this.generateJwt(user),
+
+            }
+        }
+      }
+
+
 }
